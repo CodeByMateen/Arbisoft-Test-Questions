8750bb2602305eb5b82d306b3a8bc380135f9168
Mateen Shahzad <142880053+CodeByMateen@users.noreply.github.com>
2023-12-03 22:27:36 +0500

    Add files via upload

---
+++ HTTP Methods, API, JSON/getMethod.py
@@ -0,0 +1,17 @@
+# payments = requests.get("https://www.jsonkeeper.com/b/9QRZ")
+
+import requests
+
+'''Get Method'''
+
+users = requests.get( "https://gist.github.com/CodeByMateen/220c5a5ff75548f819dd821e7fdc557d/raw"
+)
+
+print(users)  # prints status code 200
+
+users = users.json()
+# print(users)  # print all users
+
+# print([user for user in users if user.get('id') > 4 and user.get('id') < 8])  # print user with particular conditions
+
+print([user for user in users if user.get('name') == 'Danish Ali']) # Danish will be printed
