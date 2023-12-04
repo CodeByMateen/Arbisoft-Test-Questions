e6f0bec80db3144090a14aa91b8dd2e819650f0e
Mateen Shahzad <142880053+CodeByMateen@users.noreply.github.com>
2023-12-04 22:19:50 +0500

    Add files via upload

---
+++ HTTP Methods, API, JSON/04_putMethod.py
@@ -0,0 +1,18 @@
+import requests
+
+url = "https://jsonplaceholder.typicode.com/posts/64"
+
+data = {
+    "userId": 1,
+    "id": 101,
+    "title": "Test Post Updated",
+    "body": "This is an updated test post."
+}
+
+response = requests.put(url,json=data)
+if response.status_code == 200:
+  print("successful!")
+  print(response.json())
+else:
+  print("unsuccessful!")
+  print(response.json())
